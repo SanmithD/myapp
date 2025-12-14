c8cfa4901779080b4d2a0c70f0c847e58af440b2
SanmithD <sanmithdevadiga7@gmail.com>
2025-12-14 11:58:58 +0530

    v-0.8

--- vite.config.js
+++ vite.config.js
@@ -72,6 +72,7 @@ export default defineConfig({
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,svg,ico,json}"],
         navigateFallback: "/index.html",
+        updateViaCache: 'all',
       },
     }),
   ],
